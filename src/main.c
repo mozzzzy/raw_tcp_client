e26cea7551b10f7ebe1ce4afc735a2acbdf48532
mozzzzy <kurt.goedel.1109@gmail.com>
2020-07-06 22:49:30 +0900

    wip/feat: send tcp syn packet

--- src/main.c
+++ src/main.c
@@ -9,26 +9,31 @@
 #include "tcp_header/tcp_header.h"
 #include "tcp_packet/tcp_packet.h"
 
-int send_tcp_syn() {
-  return 0;
-}
-
-int main() {
-  char    *src_addr = "127.0.0.1";
+int main(int argc, char* argv[]) {
+  char    *src_addr = "172.17.0.2";
   uint16_t src_port = 49152;
-  char    *dst_addr = "127.0.0.1";
+  char    *dst_addr = "172.17.0.3";
   uint16_t dst_port = 80;
   uint32_t tcp_initial_seq = 12345678;
-  char *interface = "lo";
+  char *interface = "eth0";
 
   // build tcp SYN header
   struct tcphdr *tcphdr_syn = calloc(1, sizeof(struct tcphdr));
-  build_tcp_syn_hdr(tcphdr_syn, src_addr, src_port, dst_addr, dst_port, tcp_initial_seq);
+  build_tcp_syn_hdr(tcphdr_syn,
+                    src_addr,
+                    src_port,
+                    dst_addr,
+                    dst_port,
+                    tcp_initial_seq);
 
   // build ip header
   struct iphdr *iphdr_tcp_syn = calloc(1, sizeof(struct iphdr));
-  build_ipv4_hdr(
-      iphdr_tcp_syn, IPPROTO_TCP, src_addr, dst_addr, tcphdr_syn, sizeof(struct tcphdr));
+  build_ipv4_hdr(iphdr_tcp_syn,
+                 IPPROTO_TCP,
+                 src_addr,
+                 dst_addr,
+                 tcphdr_syn,
+                 sizeof(struct tcphdr));
 
   // build tcp SYN packet
   struct tcp_packet packet;
--- src/tcp_packet/tcp_packet.c
+++ src/tcp_packet/tcp_packet.c
@@ -1,8 +1,10 @@
+#include <arpa/inet.h>        // inet_ntop
 #include <net/ethernet.h>     // ETH_P_IP
 #include <net/if.h>           // if_nametoindex
 #include <netpacket/packet.h> // sockaddr_ll
 #include <string.h>           // memset
 
+#include "../arp/src/arp.h"
 #include "../hextet/hextet.h"
 #include "../tcp_header/tcp_header.h"
 #include "../tcp_packet/tcp_packet.h"
@@ -11,13 +13,41 @@ int send_tcp_packet(int sock, char *ifname, tcp_packet *packet) {
   struct sockaddr_ll addr;
   memset(&addr, 0, sizeof(addr));
   addr.sll_family = AF_PACKET;
-  addr.sll_protocol = htons(ETH_P_IP);  // ipv4. ipv6 is ETH_P_IPV6.
+  addr.sll_protocol = htons(ETH_P_IP);  // ipv4
   addr.sll_ifindex = if_nametoindex(ifname);
-
   addr.sll_halen = IFHWADDRLEN;
-  memset(&addr.sll_addr, 0xff, IFHWADDRLEN);  // TODO this is broadcast
+
+  // get source mac addr from interface name
+  int ret = 0;
+  uint8_t src_mac[6];
+  ret = get_mac_from_ifname(ifname, src_mac);
+  if (ret < 0) {
+    return ret;
+  }
+
+  // convert source ip uint32_t to char ptr
+  int addr_max_size = 15;
+  char src_addr[addr_max_size];
+  if (inet_ntop(AF_INET, &(packet->ip.saddr), src_addr, addr_max_size) == NULL) {
+    return -1;
+  }
+  // convert destination ip uint32_t to char ptr
+  char dst_addr[addr_max_size];
+  if (inet_ntop(AF_INET, &(packet->ip.daddr), dst_addr, addr_max_size) == NULL) {
+    return -1;
+  }
+
+  // get destination mac address
+  ret = arp_request(ifname,
+                    src_mac,
+                    src_addr,
+                    addr.sll_addr,
+                    dst_addr);
+  if (ret < 0) {
+    return ret;
+  }
 
   return sendto(
-      sock, packet, sizeof(struct tcphdr)+sizeof(struct iphdr), 0,
+      sock, (char *)packet, sizeof(struct tcphdr)+sizeof(struct iphdr), 0,
       (struct sockaddr *)&addr, sizeof(addr));
 }
